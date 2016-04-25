030e01daf239c4aaa84ec2bc03258bb46a8aa844
root <bradj47@gmail.com>
2016-04-25 16:41:30 -0600

    Finished creating all basic functions

--- vultr.php
+++ vultr.php
@@ -84,21 +84,56 @@ class vultr_api extends http_requests {
   parent::get("https://api.vultr.com/v1/regions/list");
  }
  
- // reservedip/attach
+ public function reservedip_attach (string $ip_address, int $attach_subid) {
+  $args = array(
+   'ip_address'=>$ip_address,
+   'attach_SUBID'=>$attach_subid,
+  );
+  parent::post("https://api.vultr.com/v1/reservedip/attach, $args");
+ }
  
- // reservedip/convert
+ public function reservedip_convert (array $args) {
+  if (!(array_key_exists('SUBID', $args) && array_key_exists('ip_address', $args))) {
+   exit("reservedip_convert requires arguments: SUBID and ip_address");
+  }
+  parent::post("https://api.vultr.com/v1/reservedip/convert", $args);
+ }
  
- // reservedip/create
+ public function reservedip_create (array $args) {
+  if (!(array_key_exists('DCID', $args) && array_key_exists('ip_type', $args))) {
+   exit("reservedip_create requires arguments: DCID and ip_type");
+  }
+  parent::post("https://api.vultr.com/v1/reservedip/create", $args);
+ }
  
- // reservedip/destroy
+ public function reservedip_destroy (int $subid) {
+  $args = array('SUBID'=>$subid);
+  parent::post("https://api.vultr.com/v1/reservedip/destroy", $args);
+ }
  
- // reservedip/detach
+ public function reservedip_detach (string $ip_address, int $detach_subid) {
+  $args = array(
+   'ip_address'=>$ip_address,
+   'detach_SUBID'=>$detach_subid,
+  );
+  parent::post("https://api.vultr.com/v1/reservedip/detach", $args);
+ }
  
- // reservedip/list
+ public function reservedip_list () {
+  parent::keyget("https://api.vultr.com/v1/reservedip/list");
+ }
  
- // server/app_change
+ public function server_app_change (int $subid, int $appid) {
+  $args = array(
+   'subid'=>$subid,
+   'appid'=>$appid,
+  );
+  parent::post("https://api.vultr.com/v1/server/app_change_list", $args);
+ }
  
- // server/app_change_list
+ public function server_app_change_list (int $subid) {
+  parent::keyget("https://api.vultr.com/v1/server/app_change_list?SUBID=$subid");
+ }
  
  public function server_bandwidth (int $subid) {
   parent::keyget("https://api.vultr.com/v1/server/bandwidth?SUBID=$subid");
@@ -111,22 +146,43 @@ class vultr_api extends http_requests {
   parent::post("https://api.vultr.com/v1/server/create", $args);
  }
  
- // server/create_ipv4
+ public function server_create_ipv4 (int $subid, string $reboot) {
+  $args = array(
+   'subid'=>$subid,
+   'reboot'=>$reboot,
+  );
+  parent::post("https://api.vultr.com/v1/server/create_ipv4", $args);
+ }
  
  public function server_destroy (int $subid) {
   $subid = array('SUBID' => $subid);
   parent::post("https://api.vultr.com/v1/server/destroy", $subid);
  }
  
- // server/destroy_ipv4
+ public function server_destroy_ipv4 (int $subid, string $ip) {
+  $args = array(
+   'SUBID'=>$subid,
+   'ip'=>$ip,
+  );
+  parent::post("https://api.vultr.com/v1/server/destroy_ipv4", $args);
+ }
  
  public function server_get_user_data (int $subid) {
   parent::keyget("https://api.vultr.com/v1/server/get_user_data?SUBID=$subid");
  }
  
- // server/halt
+ public function server_halt (int $subid) {
+  $subid = array('SUBID'=>$subid);
+  parent::post("https://api.vultr.com/v1/server/halt", $subid);
+ }
  
- // server/label_set
+ public function server_label_set (int $subid, string $label) {
+  $args = array(
+   'SUBID'=>$subid,
+   'label'=>$label,
+  );
+  parent::post("https://api.vultr.com/v1/server/label_set", $args);
+ }
  
  public function server_list () {
   parent::keyget("https://api.vultr.com/v1/server/list");
@@ -144,79 +200,199 @@ class vultr_api extends http_requests {
   parent::keyget("https://api.vultr.com/v1/server/neighbors?SUBID=$subid");
  }
  
- // server/os_change
+ public function server_os_change (int $subid, int $osid) {
+  $args = array(
+   'SUBID'=>$subid,
+   'OSID'=>$osid,
+  );
+  parent::post("https://api.vultr.com/v1/server/os_change", $args);
+ }
  
  public function server_os_change_list (int $subid) {
   parent::keyget("https://api.vultr.com/v1/server/os_change_list?SUBID=$subid");
  }
  
- // server/reboot
+ public function server_reboot (int $subid) {
+  $subid = array('SUBID'=>$subid);
+  parent::post("https://api.vultr.com/v1/server/reboot", $subid);
+ }
  
- // server/reinstall
+ public function server_reinstall (int $subid) {
+  $subid = array('SUBID'=>$subid);
+  parent::post("https://api.vultr.com/v1/server/reinstall", $subid);
+ }
  
- // server/restore_backup
+ public function server_restore_backup (int $subid, string $backupid) {
+  $args = array(
+   'SUBID'=>$subid,
+   'BACKUPID'=>$backupid,
+  );
+  parent::post("https://api.vultr.com/v1/server/restore_backup", $args);
+ }
  
- // server/restore_snapshot
+ public function server_restore_snapshot (int $subid, int $snapshotid) {
+  $args = array(
+   'SUBID'=>$subid,
+   'SNAPSHOTID'=>$snapshotid,
+  );
+  parent::post("https://api.vultr.com/v1/server/restore_snapshot", $args);
+ }
  
- // server/reverse_default_ipv4
+ public function server_reverse_default_ipv4 (int $subid, string $ip) {
+  $args = array(
+   'SUBID'=>$subid,
+   'ip'=>$ip,
+  );
+  parent::post("https://api.vultr.com/v1/server/reverse_default_ipv4", $args);
+ }
  
- // server/reverse_delete_ipv6
+ public function server_reverse_delete_ipv6 (int $subid, string $ip) {
+  $args = array(
+   'SUBID'=>$subid,
+   'ip'=>$ip,
+  );
+  parent::post("https://api.vultr.com/v1/server/reverse_delete_ipv6", $args);
+ }
  
  public function server_reverse_list_ipv6 (int $subid) {
   parent::keyget("https://api.vultr.com/v1/server/reverse_list_ipv6?SUBID=$subid");
  }
  
- // server/reverse_set_ipv4
+ public function server_reverse_set_ipv4 (int $subid, string $ip, string $entry) {
+  $args = array(
+   'SUBID'=>$subid,
+   'ip'=>$ip,
+   'entry'=>$entry,
+  );
+  parent::post("https://api.vultr.com/v1/server/reverse_set_ipv4", $args);
+ }
  
- // server/reverse_set_ipv6
+ public function server_reverse_set_ipv6 (int $subid, string $ip, string $entry) {
+  $args = array(
+   'SUBID'=>$subid,
+   'ip'=>$ip,
+   'entry'=>$entry,
+  );
+  parent::post("https://api.vultr.com/v1/server/reverse_set_ipv6", $args);
+ }
+
  
- // server/set_user_data
+ public function server_set_user_data (int $subid, string $userdata) {
+  $args = array(
+   'SUBID'=>$subid,
+   'userdata'=>$userdata,
+  );
+  parent::post("https://api.vultr.com/v1/server/set_user_data", $args);
+ }
  
- // server/start
+ public function server_start (int $subid) {
+  $args = array('SUBID'=>$subid);
+  parent::post("https://api.vultr.com/v1/server/start", $args);
+ }
  
- // server/upgrade_plan
+ public function server_upgrade_plan (int $subid, int $vpsplanid) {
+  $args = array(
+   'SUBID'=>$subid,
+   'VPSPLANID'=>$vpsplanid,
+  );
+  parent::post("https://api.vultr.com/v1/server/upgrade_plan", $args);
+ }
  
  public function server_upgrade_plan_list (int $subid) {
   parent::keyget("https://api.vultr.com/v1/server/upgrade_plan_list?SUBID=$subid");
  }
  
- // snapshot/create
+ public function snapshot_create (array $args) {
+  if (!(array_key_exists('SUBID', $args))) {
+   exit("snapshot_create requires argument: SUBID");
+  }
+  parent::post("https://api.vultr.com/v1/snapshot/create", $args);
+ }
  
- // snapshot/destroy
+ public function snapshot_destroy (string $snapshotid) {
+  $snapshotid = array('SNAPSHOTID'=>$snapshotid);
+  parent::post("https://api.vultr.com/v1/snapshot/destroy", $snapshotid);
+ }
  
  public function snapshot_list () {
   parent::keyget("https://api.vultr.com/v1/snapshot/list");
  }
  
- // sshkey/create
+ public function sshkey_create (string $name, string $ssh_key) {
+  $args = array(
+   'name'=>$name,
+   'ssh_key'=>$ssh_key,
+  );
+  parent::post("https://api.vultr.com/v1/sshkey/create", $args);
+ }
  
- // sshkey/destroy
+ public function sshkey_destroy (string $sshkeyid) {
+  $sshkeyid = array('SSHKEYID'=>$sshkeyid);
+  parent::post("https://api.vultr.com/v1/sshkey/destroy", $sshkeyid);
+ }
  
  public function sshkey_list () {
   parent::keyget("https://api.vultr.com/v1/sshkey/list");
  }
  
- // sshkey/update
+ public function sshkey_update (array $args) {
+  if (!(array_key_exists('SSHKEYID', $args))) {
+   exit("sshkey_update requires argument: SSHKEYID");
+  }
+  parent::post("https://api.vultr.com/v1/sshkey/update", $args);
+ }
  
- // startupscript/create
+ public function startupscript_create (string $name, string $script, string $type) {
+  $args = array(
+   'name'=>$name,
+   'script'=>$script, 
+   'type'=>$type,
+  );
+  parent::post("https://api.vultr.com/v1/startupscript/create", $args);
+ }
  
- // startupscript/destroy
+ public function startupscript_destroy (string $scriptid) {
+  $scriptid = array('SCRIPTID'=>$scriptid);
+  parent::post("https://api.vultr.com/v1/startupscript/destroy");
+ }
  
  public function startupscript_list () {
   parent::keyget("https://api.vultr.com/v1/startupscript/list");
  }
  
- // startupscript/update
+ public function startupscript_update ($args) {
+  if (!(array_key_exists('SCRIPTID', $args))) { 
+   exit("startupscript_update requires argument: SCRIPTID");
+  }
+  parent::post("https://api.vultr.com/v1/startupscript/update", $args);
+ }
  
- // user/create
+ public function user_create (string $email, string $name, string $password, string $api_enabled, array $acls) {
+  $args = array(
+   'email'=>$email,
+   'name'=>$name,
+   'password'=>$password,
+   'api_enabled'=>$api_enabled,
+   'acls'=>$acls,
+  );
+  parent::post("https://api.vultr.com/v1/user/create", $args);
+ }
  
- // user/delete
+ public function user_delete (int $userid) {
+  $userid = array('USERID'=>$userid);
+  parent::post("https://api.vultr.com/v1/user/delete", $userid);
+ }
  
  public function user_list () {
   parent::keyget("https://api.vultr.com/v1/user/list");
  }
  
- // user/update
+ public function user_update (array $args) {
+  if (!(array_key_exists('USERID', $args))) {
+   exit("user_update requires argument: USERID");
+  }
+  parent::post("https://api.vultr.com/v1/user/update", $args);
+ }
  
  // Functions that combine the above basic ones
  
